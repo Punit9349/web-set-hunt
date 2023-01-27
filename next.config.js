7a75c99a19b573cab83187bdf3ab44a6b4371b1a
sai surya <max1892@localhost.localdomain>
2023-01-27 18:29:42 +0530

    adding image fetching domains

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     JWT_SECRET:process.env.JWT_SECRET,
     START_TIME:process.env.START_TIME,
     END_TIME:process.env.END_TIME
+  },
+  images:{
+    domains:['ik.imagekit.io/3j67hddeg','web-set-hunt.vercel.app']
   }
 }
 
